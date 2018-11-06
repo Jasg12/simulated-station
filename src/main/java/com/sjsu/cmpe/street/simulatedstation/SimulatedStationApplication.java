1e2c435f2f96f2cb7dae9b59cdcba09e9eacdbc2
Aliaksei Matsarski <alex.matsarski@getzephyr.com>
2018-11-05 22:05:05 -0800

    Init project

---
+++ src/main/java/com/sjsu/cmpe/street/simulatedstation/SimulatedStationApplication.java
@@ -0,0 +1,12 @@
+package com.sjsu.cmpe.street.simulatedstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatedStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatedStationApplication.class, args);
+	}
+}
