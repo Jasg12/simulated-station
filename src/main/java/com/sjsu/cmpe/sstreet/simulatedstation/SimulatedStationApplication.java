dc27c9b29b92b8ecd58dd88e4ea44fe689c674c5
aliaksei-matsarski <aliaksei.matsarski@sjsu.edu>
2018-12-01 18:55:26 -0800

    Add logger bean

--- src/main/java/com/sjsu/cmpe/sstreet/simulatedstation/SimulatedStationApplication.java
+++ src/main/java/com/sjsu/cmpe/sstreet/simulatedstation/SimulatedStationApplication.java
@@ -1,13 +1,24 @@
 package com.sjsu.cmpe.sstreet.simulatedstation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class SimulatedStationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimulatedStationApplication.class, args);
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+	public Logger logger(){
+		return LoggerFactory.getLogger("application");
 	}
 
 }
